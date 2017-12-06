Fix Batch Normalization

nn_transfer/test/architectures/simplenet.py
@@ -5,7 +5,7 @@ import keras
 from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 
 K.set_image_data_format('channels_first')
 
@@ -14,10 +14,11 @@ class SimpleNetPytorch(nn.Module):
     def __init__(self):
         super(SimpleNetPytorch, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
+        self.bn = nn.BatchNorm2d(6)
         self.fc1 = nn.Linear(6 * 14 * 14, 10)
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
+        out = F.relu(self.bn(self.conv1(x)))
         out = F.max_pool2d(out, 2)
         out = out.view(out.size(0), -1)
         out = self.fc1(out)
@@ -30,6 +31,7 @@ def simplenet_keras():
                      activation='relu',
                      input_shape=(1, 32, 32),
                      name='conv1'))
+    model.add(BatchNormalization(axis=1, name='bn'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(10, activation=None, name='fc1'))